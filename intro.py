d86ba07281535b586e7aa37352302f75292327ba
Beeravalli Naga Shesha Siva Reddy <beeravallinag.reddy@hcl.com>
2023-08-19 10:20:27 +0530

    python introduction file commited

---
+++ intro.py
@@ -0,0 +1,33 @@
+n1 = 1
+n2 = 2
+n3 = 3
+s1 = "Hello"
+s2 = "world"
+total = n1 + n2 + n3
+print("sum of all numbers: ",total)
+n2 = n2+10
+print (n2)
+n2 += n2 + 1
+print (n2)
+n2 += 10
+print(n2)
+n2 /= 2
+print(n2)
+s2 = s1 + s2
+s2 += "hi"
+print(s2)
+print("data type of ", n2 , "is", type(n2))
+print(5j * 5j)
+print(0b101)
+print("python"+'is'+'''awesome''')
+print ("python" * 3)
+
+Q="what did you had for lunch?"
+#A=input()
+#print("you had " + A + " that's nice")
+
+my_var = "barks"
+print(f'my dog {my_var} !')
+
+print("hello \t world")
+
